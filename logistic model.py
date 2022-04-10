320429f21c16621108a7b437f1ef5cd69668cab4
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-04-10 21:58:45 +0200

    Update logistic model.py

--- logistic model.py
+++ logistic model.py
@@ -10,4 +10,27 @@ class LogisticRegression():
         self.m, self.n = X.shape
 
     def train(self, X, y):
-        
+        self.weights = np.zeros((self.n, 1))
+        self.bias = 0
+
+        for i in range(self.num_iters+1):
+            # calculate hypothesis
+            y_predict = self.sigmoid(np.dot(X, self.weights) + self.bias)
+
+            # calculate cost
+            cost = -1/self.m * np.sum(y * np.log(y_predict) + (1 - y) * np.log(1 - y_predict))
+
+            # backprop
+            dw = 1/self.m * np.dot(X.T, (y_predict - y))
+            db = 1/self.m * np.sum(y_predict - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+            if i % 1000 == 0:
+                print(f'Cost after iteration {i}: {cost}')
+              
+        return self.weights, self.bias
+
+    
+    def predict
