c704d611ee0552218f3bbc6192c1c7d7a0b16d16
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-04-10 23:46:31 +0200

    Update model_logistic.py

--- model_logistic.py
+++ model_logistic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.datasets import make_blobs
+from sklearn.datasets import make_classification
 
 class LogisticRegression():
     def __init__(self, X, learning_rate = 0.1, num_iters = 10000):
@@ -8,8 +9,7 @@ class LogisticRegression():
         self.num_iters = num_iters
 
      # m for training examples, n for features
-        self.m = self.m 
-        self.n = X.shape
+        self.m, self.n = X.shape
 
     def sigmoid(self, z):
         
@@ -21,7 +21,7 @@ class LogisticRegression():
 
 
     def train(self, X, y):
-        self.weights = np.zeros((self.n, 1))
+        self.weights = np.zeros((self.n, 1000))
         self.bias = 0
 
         for i in range(self.num_iters+1):
@@ -36,12 +36,20 @@ class LogisticRegression():
             # calculate cost
             cost = -1/self.m * np.sum(y * np.log(y_predict) + (1 - y) * np.log(1 - y_predict))
 
-            # backprop
+            """
+               delta C (w, b)/delta w = (h(x) - y) x(j to i)
+               delta C (w, b)/ delta b = (h(x) - y)
+            """
+
+            # calculate weights and bias
             dw = 1/self.m * np.dot(X.T, (y_predict - y))
             db = 1/self.m * np.sum(y_predict - y)
 
+            
+            # Updating the parameters
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
+
 
             if i % 1000 == 0:
                 print(f'Cost after iteration {i}: {cost}')
@@ -64,13 +72,15 @@ class LogisticRegression():
     
 
 if __name__ == '__main__':
-    np.random.seed(1)
-    X, y = make_blobs(n_samples=1000, centers=2)
-    y = y[:, np.newaxis]
+    # np.random.seed(1)
+    # X, y = make_blobs(n_samples=1000, centers=2)
+    # y = y[:, np.newaxis]
 
     # data = pd.read_csv("C:\\Users\\ritth\\code\\Strive\\Logistic-Regression-grp-I-R\\diabetes.csv")
     # X = data.iloc[:, [0, 1, 2, 3, 4, 5, 6, 7]].values  
-    # y = data.iloc[:, 8].values  
+    # y = data.iloc[:, 8].values 
+    # 
+    X, y = make_classification(n_features=2,n_redundant=0, n_informative=2,n_clusters_per_class=1, random_state=1) 
 
     logreg = LogisticRegression(X)
     w, b = logreg.train(X, y)
