c914f9fe532c8fae041df5feed277deb0940c7e4
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-04-11 10:19:46 +0200

    logistic

--- model_logistic.py
+++ model_logistic.py
@@ -1,10 +1,9 @@
 import numpy as np
-import pandas as pd
 from sklearn.datasets import make_blobs
-from sklearn.datasets import make_classification
+
 
 class LogisticRegression():
-    def __init__(self, X, learning_rate = 0.1, num_iters = 10000):
+    def __init__(self, X, learning_rate, num_iters):
         self.lr = learning_rate
         self.num_iters = num_iters
 
@@ -21,7 +20,7 @@ class LogisticRegression():
 
 
     def train(self, X, y):
-        self.weights = np.zeros((self.n, 1000))
+        self.weights = np.zeros((self.n, 1))
         self.bias = 0
 
         for i in range(self.num_iters+1):
@@ -51,7 +50,7 @@ class LogisticRegression():
             self.bias -= self.lr * db
 
 
-            if i % 1000 == 0:
+            if i % 100 == 0:
                 print(f'Cost after iteration {i}: {cost}')
               
         return self.weights, self.bias
@@ -59,12 +58,18 @@ class LogisticRegression():
     
     def predict(self, X):
         y_predict = self.sigmoid(np.dot(X, self.weights) + self.bias)
-        y_predict_labels = y_predict > 0.5
+    
+        pred_class = []
+        #  y_predict > 0.5 = i > 0.5
 
-        return y_predict_labels
+        pred_class = [1 if i > 0.5 else 0 for i in y_predict]
+        
+        return np.array(pred_class)
+
+    
 
     def accuracy(self, y, y_hat):
-        accuracy = np.sum(y == y_hat) / X.shape[0]
+        accuracy = np.sum(y == y_hat) / len(y)
 
         return accuracy
 
@@ -72,17 +77,12 @@ class LogisticRegression():
     
 
 if __name__ == '__main__':
-    # np.random.seed(1)
-    # X, y = make_blobs(n_samples=1000, centers=2)
-    # y = y[:, np.newaxis]
+    np.random.seed(1)
+    X, y = make_blobs(n_samples=1000, centers=2)
+    y = y[:, np.newaxis]
 
-    # data = pd.read_csv("C:\\Users\\ritth\\code\\Strive\\Logistic-Regression-grp-I-R\\diabetes.csv")
-    # X = data.iloc[:, [0, 1, 2, 3, 4, 5, 6, 7]].values  
-    # y = data.iloc[:, 8].values 
-    # 
-    X, y = make_classification(n_features=2,n_redundant=0, n_informative=2,n_clusters_per_class=1, random_state=1) 
 
-    logreg = LogisticRegression(X)
+    logreg = LogisticRegression(X, 0.1, 1000)
     w, b = logreg.train(X, y)
     y_predict = logreg.predict(X)
     acc = logreg.accuracy(y, y_predict)
